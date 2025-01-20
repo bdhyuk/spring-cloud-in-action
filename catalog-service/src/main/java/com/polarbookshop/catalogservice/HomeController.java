4329ab1a8aedd5c068dcaae20196d916d0efcc12
bdhyuk <bdhyuk@gmail.com>
2025-01-20 19:01:22 +0900

    설정관련 기능 추가

--- catalog-service/src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ catalog-service/src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -1,12 +1,21 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class HomeController {
+
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
     @GetMapping("/")
     public String getGretting() {
-        return "도서 카탈로그에 오신 것을 환영합니다.";
+        return polarProperties.getGreeting();
     }
+
 }
